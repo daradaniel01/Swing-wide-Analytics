19b0d0fa78a135e1c507609d0775daf8375a7b12
daradaniel01 <daradanie.07@gmail.com>
2025-02-17 17:10:29 +0100

    M.

--- testimony.js
+++ testimony.js
@@ -31,3 +31,20 @@
 	}
 	
 })(window.jQuery);
+
+// filter using javascript
+$(document).ready(function () {
+	$(".filter-item").click(function () {
+	  const value = $(this).attr("data-filter");
+	  if (value == "all") {
+		$(".post").show("1000");
+	  } else {
+		$(".post")
+		  .not("." + value)
+		  .hide("1000");
+		$(".post")
+		  .filter("." + value)
+		  .show("1000");
+	  }
+	});
+  });
